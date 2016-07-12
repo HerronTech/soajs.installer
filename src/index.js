ada2eed5c6b15c9928981a94bed359ff8fd55555
antoinehage <me@antoinehage.com>
2016-07-12 10:37:35 -0400

    added some trial code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var app = require('express')(),
+    swig = require('swig');
+
+// This is where all the magic happens!
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// Swig will cache templates for you, but you can disable
+// that and use Express's caching instead, if you like:
+app.set('view cache', false);
+// To disable Swig's cache, do the following:
+swig.setDefaults({ cache: false });
+// NOTE: You should always cache templates in a production environment.
+// Don't leave both of these to `false` in production!
+
+app.get('/', function (req, res) {
+    res.render('index', { /* template locals context */ });
+});
+
+app.listen(1337);
+console.log('Application Started on http://localhost:1337/');
