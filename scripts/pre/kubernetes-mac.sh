f6f3b11a7499f2add4a0a376b5b8b856110f119a
Ameer Faraj <faraj.ameer@gmail.com>
2017-06-22 17:20:00 +0300

    Updated minikube version installed via pre script

--- scripts/pre/kubernetes-mac.sh
+++ scripts/pre/kubernetes-mac.sh
@@ -1,3 +1,3 @@
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.12.2/minikube-darwin-amd64
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.19.1/minikube-darwin-amd64
 chmod +x minikube
 sudo mv minikube /usr/local/bin/
