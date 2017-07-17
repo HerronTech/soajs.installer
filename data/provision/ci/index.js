6673f75940949b6116ba0a0163b520c8f4a9ea6f
Peter <peter_eid@live.com>
2017-07-17 18:19:40 +0300

    Added Java CI Recipe.

--- data/provision/ci/index.js
+++ data/provision/ci/index.js
@@ -24,5 +24,13 @@ module.exports = [
 		"locked": true,
 		"recipe": "sudo: required\ngroup: deprecated-2017Q2\nlanguage: node_js\nnode_js: 6.9.5\nservices:\n    - elasticsearch\nenv:\n    - CXX=g++-4.8\nbranches:\n    only:\n        - master\naddons:\n    apt:\n        sources:\n            - ubuntu-toolchain-r-test\n        packages:\n            - g++-4.8\n    hosts:\n        - localhost\n        - dev-controller\nbefore_install:\n    - 'sudo apt-get update && sudo apt-get install make g++'\nbefore_script:\n    - 'npm install -g grunt-cli'\nscript:\n    - 'grunt coverage'\nafter_success:\n    - 'node ./soajs.cd.js'\n",
 		"sha": "18bba2c913e8feb3a0e206c5e2910e7e616f26aa"
+	},
+	{
+		"name": "Java Recipe",
+		"provider": "travis",
+		"type": "recipe",
+		"locked": true,
+		"recipe": "language: java\nsudo: false\ninstall: true\njdk: oraclejdk8\nafter_success:\n    - 'node ./soajs.cd.js'\n",
+		"sha": "e38700cbc221e270a3c3dc015e40904db3a0221b"
 	}
 ];
