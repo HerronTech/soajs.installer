5861a87b705bf7d0f26d71a38499a3b179d4198a
Ameer Faraj <faraj.ameer@gmail.com>
2017-08-07 20:18:28 +0300

    Updated kubernetes init script

--- scripts/pre/kubernetes-linux.sh
+++ scripts/pre/kubernetes-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This script installs prerequisites that enable this machine to join a docker swarm
+#This script installs prerequisites that enable this machine to join a kubernetes cluster
 #Compatible with Ubuntu
 
 #Need to run as root in order to install Docker
@@ -67,12 +67,14 @@ function initKubernetes(){
     kubeadm reset
     systemctl start kubelet.service
     kubeadm init --pod-network-cidr=10.244.0.0/16
-    kubectl taint nodes --all dedicated-
+    kubectl taint nodes --all node-role.kubernetes.io/master:NoSchedule-
     mkdir -p $HOME/.kube
     cp -rf /etc/kubernetes/admin.conf $HOME/.kube/config
     chown "${SUDO_USER}:${SUDO_USER}" $HOME/.kube/config
     kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel-rbac.yml
     kubectl apply -f flannel.yml
+
+    kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin --user=admin --user=kubelet --group=system:serviceaccounts;
 }
 #Start here########
 installTools
