eaf7489816ee763a9e1afb52cf41c4db5d700e50
antoinehage <me@antoinehage.com>
2019-03-18 08:47:39 -0400

    added devop and devel groups

---
+++ data/provision/urac/groups/devel.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var devel = {
+	"locked": true,
+	"code": "devel",
+	"name": "Developer Group",
+	"description": "this is the developer group",
+    "config": {
+        "allowedPackages": {
+            "DSBRD": [
+                "DSBRD_DEVEL"
+            ]
+        }
+    },
+	"tenant":{
+		"id": '5c0e74ba9acc3c5a84a51259',
+		"code": 'DBTN'
+	}
+};
+
+module.exports = devel;
---
+++ data/provision/urac/groups/devop.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var devop = {
+	"locked": true,
+	"code": "devop",
+	"name": "Devops Group",
+	"description": "this is the devops group",
+    "config": {
+        "allowedPackages": {
+            "DSBRD": [
+                "DSBRD_DEVOP"
+            ]
+        }
+    },
+	"tenant":{
+		"id": '5c0e74ba9acc3c5a84a51259',
+		"code": 'DBTN'
+	}
+};
+
+module.exports = devop;
