3d08678d3927b81b778726afaab8c136c610dc1f
antoinehage <me@antoinehage.com>
2019-10-09 18:20:48 -0400

    first commit

--- etc/config.js
+++ etc/config.js
@@ -1,5 +1,6 @@
 "use strict";
 
 module.exports = {
-	workingDirectory : ''
+	workingDirectory : '',
+	version: ''
 };
--- libexec/migrate/config.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-module.exports = ["to_urac_v2", "to_acl_v2", "to_dashboard_v1", "to_multitenant_v1"];
